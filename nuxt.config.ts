e038172a489ac08343b4ad098bf569cd274678ea
Habib Elias <149260750+HabibElias@users.noreply.github.com>
2025-08-14 11:41:59 -0900

    :sparkle: tailwindcss / shadcn / darkmode (#3)
    * :sparkle: tailwindcss / shadcn / darkmode
    
    * :bug: fix bug in GitHub Action instaling pnpm
    
    * :bug: fix bug in GitHub Action instaling pnpm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,38 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/icon", "@nuxt/image", "@nuxt/eslint"],
+  modules: [
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/eslint",
+    "shadcn-nuxt",
+  ],
   eslint: {
     config: {
       standalone: false,
     },
+  },
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
+  alias: {
+    "@": ".",
   },
 });
