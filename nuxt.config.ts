5e0115131537ad36a5474fc50bf6661745b730cc
Habib Elias <habibelias234@gmail.com>
2025-08-17 00:03:25 -0900

    :sparkles: typesafe env setup / zod

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath } from "node:url";
 
+import "./lib/env";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
