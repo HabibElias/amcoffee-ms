23eb645f0c9179941fb32fe318bd67548632be32
Habib Elias <149260750+HabibElias@users.noreply.github.com>
2025-08-17 18:09:13 -0900

    :hummer: auth (#11)

---
+++ lib/db/schema/auth-schema.ts
@@ -0,0 +1,65 @@
+import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const user = sqliteTable("user", {
+  id: int().primaryKey({ autoIncrement: true }),
+  name: text().notNull(),
+  email: text().notNull().unique(),
+  emailVerified: int({ mode: "boolean" })
+    .$defaultFn(() => false)
+    .notNull(),
+  image: text(),
+  createdAt: int()
+    .$defaultFn(() => /* @__PURE__ */Date.now())
+    .notNull(),
+  updatedAt: int()
+    .$defaultFn(() => /* @__PURE__ */Date.now())
+    .notNull(),
+});
+
+export const session = sqliteTable("session", {
+  id: int().primaryKey({ autoIncrement: true }),
+  expiresAt: int().notNull(),
+  token: text().notNull().unique(),
+  createdAt: int().notNull(),
+  updatedAt: int().notNull(),
+  ipAddress: text(),
+  userAgent: text(),
+  userId: text()
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+});
+
+export const account = sqliteTable("account", {
+  id: int().primaryKey({ autoIncrement: true }),
+  accountId: text().notNull(),
+  providerId: text().notNull(),
+  userId: text()
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+  accessToken: text(),
+  refreshToken: text(),
+  idToken: text(),
+  accessTokenExpiresAt: int("access_token_expires_at", {
+    mode: "timestamp",
+  }),
+  refreshTokenExpiresAt: int("refresh_token_expires_at", {
+    mode: "timestamp",
+  }),
+  scope: text(),
+  password: text(),
+  createdAt: int().notNull(),
+  updatedAt: int().notNull(),
+});
+
+export const verification = sqliteTable("verification", {
+  id: int().primaryKey({ autoIncrement: true }),
+  identifier: text().notNull(),
+  value: text().notNull(),
+  expiresAt: int().notNull(),
+  createdAt: int().$defaultFn(
+    () => /* @__PURE__ */Date.now(),
+  ),
+  updatedAt: int().$defaultFn(
+    () => /* @__PURE__ */Date.now(),
+  ),
+});
