175054c28717782b0c9dc182c1d5fb6a768b636a
Habib Elias <habibelias234@gmail.com>
2025-08-18 08:47:57 -0900

    :hammer: fixxed lint error

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,9 +2,7 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 import db from "./db/index"; // your drizzle instance
-
 import env from "./env";
-
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
