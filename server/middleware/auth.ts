556df556876c79d1d55a05e52a53a8a3f5c9c083
Habib Elias <habibelias234@gmail.com>
2025-09-13 11:51:23 -0900

    feat: add a menu item to the menu

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,17 +1,17 @@
 import { auth } from "~~/lib/auth";
 
 export default defineEventHandler(async (event) => {
-  if (event.path.startsWith("/dashboard")) {
-    const session = await auth.api.getSession({ headers: event.headers });
+  const session = await auth.api.getSession({ headers: event.headers });
 
-    if (!session) {
+  event.context.user = session?.user;
+
+  if (event.path.startsWith("/dashboard")) {
+    if (!session?.user) {
       await sendRedirect(event, "/", 302);
     }
   }
   if (event.path.startsWith("/settings")) {
-    const session = await auth.api.getSession({ headers: event.headers });
-
-    if (!session) {
+    if (!session?.user) {
       await sendRedirect(event, "/", 302);
     }
   }
